814eb94f0f7a6eae785a1e85b3d00a72e636bfc9
quanglv96 <quanglv1712@gmail.com>
2022-10-05 10:06:28 +0700

    system time

---
+++ _Modul-2/src/SystemTime.java
@@ -0,0 +1,7 @@
+import java.util.Date;//tạo 1 đối tượng date cho thời gian hiện tại
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now=new Date();
+        System.out.println("Now is: "+ now);
+    }
+}
