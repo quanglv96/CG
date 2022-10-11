6adfd11d1ad38dcd85a28bb18e7dabe623cd2d65
quanglv96 <quanglv1712@gmail.com>
2022-10-11 08:30:18 +0700

    update re size

--- _Modul-2/src/Abstract_interface/Resizeable/test.java
+++ _Modul-2/src/Abstract_interface/Resizeable/test.java
@@ -6,7 +6,7 @@ public class test {
         shapes[0] = new Circle(3.5, "indigo", false);
         shapes[1] = new Rectangle(2.5, 3.8, "orange", true);
         for(int i=0; i< shapes.length;i++){
-            System.out.println(shapes.toString());
+            System.out.println(shapes[i].toString());
         }
         for(int i=0;i< shapes.length;i++){
             if(i==0) {
